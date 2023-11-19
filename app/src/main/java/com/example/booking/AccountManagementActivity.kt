14340e347646091be5ec77d95e00fa3918feced0
teodord25 <djuric.teodor25@gmail.com>
2023-11-19 17:25:48 +0100

    implement account management activity

---
+++ app/src/main/java/com/example/booking/AccountManagementActivity.kt
@@ -0,0 +1,23 @@
+package com.example.booking
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.example.booking.databinding.ActivityAccountManagementBinding
+
+class AccountManagementActivity : ComponentActivity() {
+    private lateinit var binding: ActivityAccountManagementBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAccountManagementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConfirmInfoChange.setOnClickListener {
+            val fname = binding.editTextFirstName.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            Toast.makeText(this, "First Name: $fname, Password: $password", Toast.LENGTH_SHORT).show()
+            // business logic
+        }
+    }
+}
