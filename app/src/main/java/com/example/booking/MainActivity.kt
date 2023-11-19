9f9bd6c1aeb9fce618e08981399149fd4aab1cbe
teodord25 <djuric.teodor25@gmail.com>
2023-11-19 15:49:27 +0100

    convert main activity and project files for viewbinding instead of initial jetpack compose

--- app/src/main/java/com/example/booking/MainActivity.kt
+++ app/src/main/java/com/example/booking/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.example.booking
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.booking.ui.theme.BookingTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.booking.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BookingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Assuming you have a button in your MainActivity that starts the AccountActivity
+        // binding.startAccountActivityButton.setOnClickListener {
+        //     val intent = Intent(this, AccountManagementActivity::class.java)
+        //     startActivity(intent)
+        // }
     }
 }
