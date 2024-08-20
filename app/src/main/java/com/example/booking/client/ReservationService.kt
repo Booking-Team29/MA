01bcd5dafb8a9239b6e396858a3aa0c0d6994105
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-08-20 09:43:24 +0200

    feat: reservation models and service

--- app/src/main/java/com/example/booking/client/ReservationService.kt
+++ app/src/main/java/com/example/booking/client/ReservationService.kt
@@ -1,6 +1,8 @@
 package com.example.booking.client
 
 import com.example.booking.model.Accommodation.AccommodationFilterDTO
+import com.example.booking.model.Reservation.Reservation
+import com.example.booking.model.Reservation.ReservationRequest
 import com.example.booking.model.Reservation.ReservationRequestDTO
 import retrofit2.Call
 import retrofit2.http.Body
@@ -17,4 +19,18 @@ interface ReservationService {
     )
     @POST("reservation")
     fun makeReservation(@Body reservationRequestDTO: ReservationRequestDTO) : Call<Void>
+
+    @Headers(
+        "User-Agent: Mobile-Android",
+        "Content-Type: application/json",
+    )
+    @GET("reservation")
+    fun getReservations() : Call<List<Reservation>>
+
+    @Headers(
+        "User-Agent: Mobile-Android",
+        "Content-Type: application/json",
+    )
+    @GET("reservation/reservationRequests")
+    fun getReservationRequests() : Call<List<ReservationRequest>>
 }
